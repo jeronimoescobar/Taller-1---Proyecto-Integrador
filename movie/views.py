1e04dcb3f5d53258ddfb1c423125ca168344e6bb
Jeronimo Escobar <jeroescobarr@gmail.com>
2026-07-28 11:34:35 -0500

    nuevo archivo about.html

--- movie/views.py
+++ movie/views.py
@@ -6,3 +6,5 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html', {'name':'Jeronimo Escobar'})
     
+def about(request):
+    return render(request, 'about.html')
